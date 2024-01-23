599b92aba54abd5e5f14f3b588813fa618971ca5
annerachael <annerwenji@gmail.com>
2024-01-23 08:33:33 -0800

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp/school':
+file { 'school':
   ensure  => present,
   mode    => '0744',
   owner   => www-data,
   group   => www-data,
   content => 'I love puppet',
+  path    => '/tmp/school',
 }
